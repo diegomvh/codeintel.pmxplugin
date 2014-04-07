a589abb512c8e9911f99af4ef5aaf1de23caf4eb
diegomvh <diegomvh@gmail.com>
2014-04-07 18:15:50 -0300

    Add libs and arch to sys.path

--- codeintel/__init__.py
+++ codeintel/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from codeintel.tools.codeintel2.manager import Manager
+import os
+import sys
 
-def registerPlugin(manager):
-    pass
+def registerPlugin(manager, descriptor):
+    libs_path = os.path.join(descriptor.path, 'libs')
+    if libs_path not in sys.path:
+        sys.path.insert(0, libs_path)
+    
+    arch_path = os.path.join(descriptor.path, 'arch')
+    if arch_path not in sys.path:
+        sys.path.insert(0, arch_path)
+    
+    from codeintel2.manager import Manager
