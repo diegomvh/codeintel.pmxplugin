2ea1d8ba068f4f29f0dcf3ea1a70e88a51ba904d
diegomvh <diegomvh@gmail.com>
2014-07-07 13:13:11 -0300

    Update pythoncile

--- libs/codeintel2/common.py
+++ libs/codeintel2/common.py
@@ -34,6 +34,7 @@
 # the terms of any one of the MPL, the GPL or the LGPL.
 #
 # ***** END LICENSE BLOCK *****
+from __future__ import unicode_literals
 
 """Code Intelligence: common definitions"""
 # Dev Notes:
