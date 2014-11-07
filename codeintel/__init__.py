80516fdc26746c91f7d86343c947fc113eda1222
Diego van Haaster <diegomvh@gmail.com>
2014-11-07 07:48:06 -0300

    Comment completer

--- codeintel/__init__.py
+++ codeintel/__init__.py
@@ -5,4 +5,5 @@ from prymatex.gui.codeeditor import CodeEditor
 from codeintel.addons import CodeIntelAddon
 
 def registerPlugin(manager, descriptor):
-    manager.registerComponent(CodeIntelAddon, CodeEditor)
+    pass
+    #manager.registerComponent(CodeIntelAddon, CodeEditor)
