5f70e77fd086f6a175d4870170429859746017aa
Diego van Haaster <diegomvh@gmail.com>
2015-02-02 16:51:39 -0300

    Selections

--- libs/sublime/selection.py
+++ libs/sublime/selection.py
@@ -1,33 +1,77 @@
 #!/usr/bin/env python
 
+from bisect import bisect
+import functools
+
+#from .region import Region
+import region
+
 class Selection(object):
+    __slots__ = ('_regions', )
     """Maintains a set of Regions, ensuring that none overlap. The regions are kept in sorted order. 
     """
     def __init__(self):
         super(Selection, self).__init__()
-        self.regions = []
+        self._regions = []
 
+    # ------------- Magics
+    def __str__(self):
+        return "<%s>" % ",".join(map(str, self._regions))
+        
     def clear(self):
         """clear()	None	Removes all regions.
         """
-        pass
+        self._regions = []
 
+    def _add(self, regions, region):
+        if regions:
+            while regions and regions[-1].intersects(region):
+                r = regions.pop()
+                region = r.cover(region)
+        regions.append(region)
+        return regions
+        
     def add(self, region):
         """add(region)	None	Adds the given region. It will be merged with any intersecting regions already contained within the set.
         """
-        pass
-
+        index = bisect(self._regions, region)
+        self._regions.insert(index, region)
+        self._regions = functools.reduce(self._add, self._regions, [])
+        
     def add_all(self, region_set):
         """add_all(region_set)	None	Adds all regions in the given set.
         """
-        pass
+        for region in region_set:
+            index = bisect(self._regions, region)
+            self._regions.insert(index, region)
+        self._regions = functools.reduce(self._add, self._regions, [])
 
+    def _subtract(self, regions, region):
+        if regions:
+            while regions and regions[-1].intersects(region):
+                r = regions.pop()
+                region = r.cover(region)
+        regions.append(region)
+        return regions
+        
     def subtract(self, region):
         """subtract(region)	None	Subtracts the region from all regions in the set.
         """
-        pass
-
+        # TODO Terminar
+        index = bisect(self._regions, region)
+        self._regions.insert(index, region)
+        self._regions = functools.reduce(self._subtract, self._regions, [])
+        
     def contains(self, region):
         """contains(region)	bool	Returns true iff the given region is a subset.
         """
-        pass
+        return any(lambda r: r.contains(region), self._regions)
+
+if __name__ == '__main__':
+    s = Selection()
+    s.add(region.Region(1,10))
+    s.add(region.Region(10,110))
+    s.add(region.Region(11,100))
+    s.add(region.Region(300,101))
+    
+    print(s)
