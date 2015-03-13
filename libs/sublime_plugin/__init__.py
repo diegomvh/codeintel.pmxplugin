0aca351b528b96e03c281b8de1ab10e840ce4f37
Diego van Haaster <diegomvh@gmail.com>
2015-03-13 15:19:53 -0300

    Remove sublime lib

--- libs/sublime_plugin/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-# Sublime Plugins abstraction layer
-
-from .command import *
-from .listener import *
--- libs/sublime_plugin/command/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from .text import *
-from .window import *
-from .application import *
